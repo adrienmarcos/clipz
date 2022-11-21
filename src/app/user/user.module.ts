2b09e2638422a7d2156baaf761cc38a131588e18
Adrien Rossato <adrien.marcos@gmail.com>
2022-11-21 17:11:45 -0300

    creating the user module and the nav component

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+
+@NgModule({
+  declarations: [
+    AuthModalComponent
+  ],
+  exports: [
+    AuthModalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
