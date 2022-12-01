ec938e712bde66f5e005161d1c968c768bf1ad3c
Adrien Rossato <adrien.marcos@gmail.com>
2022-12-01 19:26:22 -0300

    breaking the register form into components

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { ModalService } from "../../services/modal.service";
 
 @Component({
@@ -6,14 +6,18 @@ import { ModalService } from "../../services/modal.service";
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
 
   @Input()
   modalID: string = '';
 
-  constructor(public modal: ModalService) {}
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   public closeModal(): void {
     this.modal.toggleModal(this.modalID);
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 }
